f346c2169525858821c0b3d353bdd5ce83da2a2e
Eugene Shevchenko <eugeneShevchenko0@gmail.com>
2025-02-27 21:54:24 +0500

    Services unit tests finished

--- tacocloud/src/test/java/com/tacocloud/jea/services/OrderServiceTest.java
+++ tacocloud/src/test/java/com/tacocloud/jea/services/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package com.tacocloud.jea.services;
 
 import com.tacocloud.data.OrderRepository;
+import com.tacocloud.data.TacoRepository;
 import com.tacocloud.domain.Taco;
 import com.tacocloud.domain.TacoOrder;
 import com.tacocloud.services.OrderService;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 public class OrderServiceTest {
@@ -56,6 +58,82 @@ public class OrderServiceTest {
 
         assertThat(size).isEqualTo(3);
         assertThat(receivedOrders).isEqualTo(orders);
+    }
+
+    @Test
+    public void shouldReturnSavedOrder() {
+        var order = testOrder();
+
+        when(orderRepository.save(any())).thenReturn(order);
+
+        var receivedOrder = orderService.saveOrder(order);
+
+        assertThat(receivedOrder).isEqualTo(order);
+    }
+
+    @Test
+    public void shouldReturnUpdatedOrder() {
+        Long id = 1L;
+        var oldOrder = testOrder();
+        oldOrder.setId(id);
+
+        var newOrder = testOrder();
+        newOrder.setDeliveryName("Delivery name changed");
+
+        when(orderRepository.findById(any())).thenReturn(Optional.of(oldOrder));
+        when(orderRepository.save(any())).thenReturn(newOrder);
+
+        var receivedOrder = orderService.updateOrder(id, newOrder);
+
+        assertThat(receivedOrder.isPresent()).isTrue();
+        assertThat(receivedOrder.get()).isEqualTo(newOrder);
+    }
+
+    @Test
+    public void shouldReturnEmptyOptionalWhenUpdatingOrder() {
+
+        when(orderRepository.findById(any())).thenReturn(Optional.empty());
+
+        var receivedOrder = orderService.updateOrder(1L, testOrder());
+
+        assertThat(receivedOrder.isPresent()).isFalse();
+    }
+
+    @Test
+    public void shouldReturnPatchedOrder() {
+        Long id = 1L;
+        var oldOrder = testOrder();
+        oldOrder.setId(id);
+
+        var newOrder = testOrder();
+        newOrder.setDeliveryName("Delivery name changed");
+        newOrder.setDeliveryState("Changed State");
+
+        when(orderRepository.findById(any())).thenReturn(Optional.of(oldOrder));
+        when(orderRepository.save(any())).thenReturn(oldOrder);
+
+        var receivedOrder = orderService.updateOrder(id, newOrder);
+
+        assertThat(receivedOrder.isPresent()).isTrue();
+        assertThat(receivedOrder.get()).isEqualTo(oldOrder);
+    }
+
+    @Test
+    public void shouldReturnEmptyOptionalWhenPatchingOrder() {
+
+        when(orderRepository.findById(any())).thenReturn(Optional.empty());
+
+        var receivedOrder = orderService.updateOrder(1L, testOrder());
+
+        assertThat(receivedOrder.isPresent()).isFalse();
+    }
+
+    @Test
+    public void shouldDoNothingWhenDeletingOrder() {
+
+        doNothing().when(orderRepository).deleteById(any());
+
+        orderService.deleteOrder(1L);
     }
 
     private TacoOrder testOrder() {
